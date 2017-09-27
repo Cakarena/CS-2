51d1e07d81b44745442a769ce995889ad390ceab
Anderson-Karena <30563088+Cakarena@users.noreply.github.com>
2017-09-27 23:04:49 +1000

    Add files via upload

---
+++ sum_double_f.py
@@ -0,0 +1,19 @@
+def sum_double(a,b):
+	a=int(a)
+	b=int(b)
+	if a == b:
+		sum = (a + b) * 2
+		print (sum)
+	elif a != b:
+		sum = (a + b)
+		print (sum)
+
+
+sum_double(1,2)
+sum_double(3,2)
+sum_double(2,2)
+sum_double(-1,0)
+sum_double(3,3)
+sum_double(0,0)
+sum_double(0,1)
+sum_double(3,4)
