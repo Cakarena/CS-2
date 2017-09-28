0671d033591029b8ac5cddf20471cfb57a4ac2df
Anderson-Karena <30563088+Cakarena@users.noreply.github.com>
2017-09-28 12:21:28 +1000

    Add files via upload

---
+++ license_f.py
@@ -0,0 +1,11 @@
+def license(age, hours):
+    if age >= 16 and hours >= 200:
+        print('issue license')
+    else:
+        print('dont issue license')
+
+license(18,250)
+license(15,192)
+license(16,199)
+license(16,201)
+license(16,200)
