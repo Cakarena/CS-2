38b811d00072e57258b2f4b11eb7b2e41d22ac83
Anderson-Karena <30563088+Cakarena@users.noreply.github.com>
2017-09-29 19:33:33 +1000

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+##Program Name: fizzbuzz.py 
+##Write a Python program which iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of the number
+## and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".Sample Output : 
+#fizzbuzz
+#1
+#2
+#fizz
+#4 
+#Buzz
+
+for i in range(0,50):
+	if i % 15 == 0:
+		print("fizzbuzz")
+	elif i % 5 == 0:
+		print("buzz")
+	elif i % 3 == 0:
+		print("fizz")
+	else:
+		print(i)
